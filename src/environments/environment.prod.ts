8bdc6340c7d25a25b4f978c6137d43bc40eedd99
Cristian <cristianjosecruzalmanza1998@gmail.com>
2022-08-27 15:26:36 -0400

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  base_url: 'https://app-api.cochabamba.bo/api',
-  base_url_public: 'https://app-api.cochabamba.bo'
+  base_url: 'https://appdev_api.cochabamba.bo/api',
+  base_url_public: 'https://appdev_api.cochabamba.bo'
+  // base_url: 'https://app-api.cochabamba.bo/api',
+  // base_url_public: 'https://app-api.cochabamba.bo'
 };
